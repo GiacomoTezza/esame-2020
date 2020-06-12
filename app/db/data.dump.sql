5b7dd9688a1f8a15268a67325cbb6b3733db19a0
GiacomoTezza <tezza.giacomo01@gmail.com>
2020-06-12 17:03:11 +0200

    minor fixies

--- app/db/data.dump.sql
+++ app/db/data.dump.sql
@@ -1,10 +1,11 @@
 INSERT INTO License (Name, Description) VALUES 
-('AM', 'Questo documento si può conseguire al raggiungimento dei 14 anni di età.'),
-('A', 'Questo documento si può conseguire non prima del compimento dei 24 anni di età o dei 20 anni qualora si sia in possesso della patente A2 da almeno due anni.'),
-('A2', 'Questo documento si può ottenere dopo aver compiuto i 18 anni di età.'),
-('B', 'Questo documento si può ottenere a partire dal compimento dei 18 anni di età.'),
-('C', 'Questo documento si può ottenere a partire dal compimento dei 21 anni di età, ma questo limite scende a 18 qualora si sostenga anche l’esame per la patente CQC trasporto cose con formazione ordinaria.'),
-('D', 'Per guidare autobus e scuolabus occorre conseguire la CQC persone ed è obbligatorio ottenere la patente B prima della D.');
+('AM', '14 anni - Ciclomotori a 2 ruote o 3 ruote, quadricicli leggeri'),
+('A1', '16 anni - Motocicli leggeri, macchine agricole'),
+('A2', '18 anni - Motocicli, macchine agricole'),
+('A', '20 anni se titolare di A2 da almeno 2 anni, 21 anni - Motocicli, macchine agricole'),
+('B1', '16 anni - Quadricicli'),
+('B', '18 anni - Quadricicli a motore, autoveicoli, macchine operatrici non eccezionali, macchine agricole'),
+('BE', '18 anni - Autoveicoli');
 
 INSERT INTO Client (Name, Surname, CF) VALUES 
 ('Michael', 'Scott', 'VFHMXD67L05I530G'),
@@ -17,6 +18,6 @@ INSERT INTO `Registration` VALUES
 (1,1,'2017-03-21','Bocciato Teoria'),
 (1,2,'2002-04-12','Promosso Pratica'),
 (3,4,'2009-10-03','Bocciato Teoria'),
-(4,3,'1999-06-12','Promosso Pratica'),
-(4,4,'2015-01-22','Promosso Pratica'),
-(4,5,'2003-11-11','Bocciato Teoria');
+(6,3,'1999-06-12','Promosso Pratica'),
+(6,4,'2015-01-22','Promosso Pratica'),
+(6,5,'2003-11-11','Bocciato Teoria');
